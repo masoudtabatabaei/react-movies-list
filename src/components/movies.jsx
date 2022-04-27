b1073f326b715e06403cda835918ce49c6a9153d
Masoud <masoudtabatabaei20@gmail.com>
2022-04-27 09:35:55 +0430

    Implement movies list table structure component

---
+++ src/components/movies.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  render() {
+    return (
+      <>
+        <table className="table mt-5" style={{ maxWidth: "800px" }}>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button className="btn btn-sm btn-danger">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default Movies;
