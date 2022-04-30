c9ed5d77c4870410a351620398742aef311abd1f
Masoud <masoudtabatabaei20@gmail.com>
2022-04-30 07:20:48 +0430

    Fix: Don't display paginate if total pages is less or equal pagesize

--- src/common/paginate.jsx
+++ src/common/paginate.jsx
@@ -8,17 +8,20 @@ const Paginate = (props) => {
         ? total / pageSize
         : Math.floor(total / pageSize) + 1;
 
-    for (let i = 0; i + 1 <= paginateLength; i++) {
-      const activePageClass = i === currentPage ? "active" : "";
-      listItems.push(
-        <li
-          key={i}
-          className={"page-item " + activePageClass}
-          onClick={() => onSelectPage(i)}
-        >
-          <a className="page-link">{i + 1}</a>
-        </li>
-      );
+    // if paginateLength is equal 1 then it don't need to display paginate
+    if (paginateLength > 1) {
+      for (let i = 0; i + 1 <= paginateLength; i++) {
+        const activePageClass = i === currentPage ? "active" : "";
+        listItems.push(
+          <li
+            key={i}
+            className={"page-item " + activePageClass}
+            onClick={() => onSelectPage(i)}
+          >
+            <a className="page-link">{i + 1}</a>
+          </li>
+        );
+      }
     }
 
     return listItems;
